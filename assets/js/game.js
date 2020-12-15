3f946d1b986d7e507cc9ae803a7797e7f85aad18
Halleys-Comet <jhalley230@gmail.com>
2020-12-15 16:21:07 -0600

    add enemy cambatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,19 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//      *Fight all enemey-robots
+//      *Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round 
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -73,4 +81,6 @@ var fight = function() {
 
 
 
-fight();
+for(var i=0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
